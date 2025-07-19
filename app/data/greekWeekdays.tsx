e6814cdf69e779f7a221e84e36521042aae73194
yourijanssen <yourijanssen123@gmail.com>
2025-07-19 15:12:43 +0200

    forget OOP weekdays.

---
+++ app/data/greekWeekdays.tsx
@@ -0,0 +1,10 @@
+
+export const greekWeekdays: [string, string][] = [
+  ["Δευτέρα", "Monday"],   ["Monday", "Δευτέρα"],
+  ["Τρίτη", "Tuesday"],    ["Tuesday", "Τρίτη"],
+  ["Τετάρτη", "Wednesday"],["Wednesday", "Τετάρτη"],
+  ["Πέμπτη", "Thursday"],  ["Thursday", "Πέμπτη"],
+  ["Παρασκευή", "Friday"], ["Friday", "Παρασκευή"],
+  ["Σάββατο", "Saturday"], ["Saturday", "Σάββατο"],
+  ["Κυριακή", "Sunday"],   ["Sunday", "Κυριακή"],
+];
